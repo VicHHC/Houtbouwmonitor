0594a5f512f470bab0ddb08bf8dbf9082563e844
VicHHC <145341812+VicHHC@users.noreply.github.com>
2023-09-20 23:20:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+    // Show or hide follow-up question based on "Do you like ice cream?" answer
     $("#q1").change(function() {
         if ($(this).val() === "yes") {
             $("#followUpQuestion").show();
@@ -8,14 +9,18 @@ $(document).ready(function() {
         }
     });
 
+    // Handle form submission
     $("#questionnaireForm").submit(function(event) {
         event.preventDefault();
 
+        // Collect form data
         var formData = {
+            "Email": $("#email").val(),
             "Ice Cream": $("#q1").val(),
             "Flavor": $("#q2").val()
         };
 
+        // Send form data to the server
         $.ajax({
             type: "POST",
             url: "https://script.google.com/macros/s/AKfycbw26thcSanEpZgVEaao7CJ8jBRGh-dSLgAfQDuCV_RjBS-R1vE-TuOQnleEirKSxCGHlQ/exec",
